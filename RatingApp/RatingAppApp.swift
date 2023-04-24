8e39780969167ed6ad9110faff4e85ac6c1264d2
Jhosel Badillo Cortes <jhosel.badillo.cortes@gmail.com>
2023-04-23 19:52:08 -0600

    Initial Commit

---
+++ RatingApp/RatingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingAppApp.swift
+//  RatingApp
+//
+//  Created by Jhosel Badillo Cortes on 23/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RatingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
